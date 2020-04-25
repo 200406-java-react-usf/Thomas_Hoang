65724e8c6cd5271a30a29fbed7db1e94eab74d9a
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-04-25 18:25:04 -0400

    Created Brands model.

---
+++ models/Brands.ts
@@ -0,0 +1,10 @@
+export class Brands{
+
+    id: number;
+    brandName: string;
+
+    constructor(id: number, brandName: string){
+        this.id = id;
+        this.brandName = brandName;
+    }
+}
