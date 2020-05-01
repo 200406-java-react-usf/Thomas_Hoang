9e7bd96407a625535597d1e0c2adf3afa1f83dfc
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-01 15:52:11 -0400

    Restructed data models.

--- models/Users.ts
+++ models/Users.ts
@@ -1,12 +1,14 @@
 export class User {
 
+    userID: number;
     username: string;
     first: string;
     last: string;
     //Multdimensional array to allow user to have multiple different products
     inventory: [[string, number, number]];
 
-    constructor(un: string, fn: string, ln: string,productName:string, productQuantity: number, personalRating?: number){
+    constructor(id: number,un: string, fn: string, ln: string, productName: string, productQuantity: number, personalRating: number){
+        this.userID = id;
         this.username = un;
         this.first = fn;
         this.last = ln;
--- models/Waxes.ts
+++ models/Waxes.ts
@@ -4,15 +4,17 @@ export class Wax{
     productName: string;
     productPrice: number;
     limitedEdition: boolean;
+    scentCategory: string;
     scentStrength: number;
-    ProductDescription: string;
+    scentDescription: string;
 
-    constructor(brandID: number, name: string, price: number, limited: boolean, scentStrength?: number, description?: string){
-        this.brandID = brandID
+    constructor(brandID: number, name: string, price: number, limited: boolean,scentCategory: string, scentStrength?: number, description?: string){
+        this.brandID = brandID;
         this.productName = name;
         this.productPrice = price;
         this.limitedEdition = limited;
+        this.scentCategory = scentCategory;
         this.scentStrength = scentStrength;
-        this.ProductDescription = description;
+        this.scentDescription = description;
     }
 }
