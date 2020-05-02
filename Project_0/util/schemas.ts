af623303d3be979867f1693e05c168abe12f2666
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-02 13:46:32 -0400

    Added 2 other schemas Wax and Brands.

---
+++ Project_0/util/schemas.ts
@@ -0,0 +1,15 @@
+export interface UserSchema {
+    id: number,
+    username: string,
+    first_name: string,
+    last_name: string,
+    role_name: string
+}
+
+export interface PostSchema {
+
+}
+
+export interface ThreadSchema {
+    
+}
