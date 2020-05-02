c059e3b8be6bbc19f4b80ecc22981146a165d4a6
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-02 19:10:29 -0400

    Created getWaxByID but no repo function to getByID.

--- Project_0/services/wax-service.ts
+++ Project_0/services/wax-service.ts
@@ -1,11 +1,46 @@
+import { Wax } from "../models/wax";
 import { WaxRepository } from "../repos/wax-repo";
+import { isValidId, isValidStrings, isValidObject, isPropertyOf, isEmptyObject } from "../util/validator";
+import {
+    BadRequestError,
+    ResourceNotFoundError,
+    NotImplementedError,
+    ResourcePersistenceError, 
+    AuthenticationError
+} from "../errors/errors";
+import { UserService } from "./user-service";
+
 
 export class WaxService {
 
-    constructor(private postRepo: WaxRepository) {
-        this.postRepo = postRepo;
+    constructor(private waxRepo: WaxRepository) {
+        this.waxRepo = waxRepo;
     }
 
-    // TODO implement service methods
+    async getAllWaxes(): Promise<Wax[]> {
+
+        let waxes = await this.waxRepo.getAll();
+
+        if (waxes.length == 0) {
+            throw new ResourceNotFoundError();
+        }
+
+        return waxes
+    }
+
+    async getWaxByID(id: number): Promise<Wax> {
+
+        if (!isValidId(id)) {
+            throw new BadRequestError();
+        }
+
+        let wax = await this.waxRepo.getByID(id);
+
+        if (isEmptyObject(wax)) {
+            throw new ResourceNotFoundError();
+        }
+
+        return wax
+    }
 
 }
