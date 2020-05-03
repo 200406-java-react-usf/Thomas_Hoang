0993334bb802ac5d389de9491574c131163c1641
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-03 15:11:52 -0400

    Add owned imports and variables into app config.

--- Project_0/config/app.ts
+++ Project_0/config/app.ts
@@ -2,6 +2,8 @@ import { UserRepository } from "../repos/user-repo";
 import { UserService } from "../services/user-service";
 import { WaxRepository } from "../repos/wax-repo";
 import { WaxService } from "../services/wax-service";
+import { OwnedRepository } from "../repos/owned-repo";
+import { OwnedService } from "../services/owned-service";
 
 const userRepo = new UserRepository();
 const userService = new UserService(userRepo);
@@ -9,7 +11,11 @@ const userService = new UserService(userRepo);
 const waxRepo = new WaxRepository();
 const waxService = new WaxService(waxRepo);
 
+const ownedRepo = new OwnedRepository();
+const ownedService = new OwnedService(ownedRepo);
+
 export default {
     userService,
-    waxService
+    waxService,
+    ownedService
 }
---
+++ Project_0/routers/owned-router.ts
@@ -0,0 +1,37 @@
+import url from 'url';
+import express from 'express';
+import AppConfig from '../config/app';
+import { isEmptyObject } from '../util/validator';
+import { ParsedUrlQuery } from 'querystring';
+
+export const OwnedRouter = express.Router();
+
+const ownedService = AppConfig.waxService;
+
+OwnedRouter.get('/', async (req, resp) => {
+    
+    try {
+
+        let reqURL = url.parse(req.url, true);
+
+        if(!isEmptyObject<ParsedUrlQuery>(reqURL.query)) {
+            let payload = await ownedService.getWaxByUniqueKey({...reqURL.query});
+            resp.status(200).json(payload);
+        } else {
+            let payload = await ownedService.getAllWaxes();
+            resp.status(200).json(payload);
+        }
+
+    } catch (e) {
+        resp.status(e.statusCode).json(e);
+    }
+});
+OwnedRouter.get('/:id', async (req, resp) => {
+    const id = +req.params.id; // the plus sign is to type coerce id into a number
+    try {
+        let payload = await ownedService.getWaxByID(id);
+        return resp.status(200).json(payload);
+    } catch (e) {
+        return resp.status(404).json(e).send();
+    }
+});
