0a14d7d7620f67e20906758a05d0491d3edcb5ec
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-03 13:29:16 -0400

    Fixed Wax-router to work now.

--- Project_0/routers/wax-router.ts
+++ Project_0/routers/wax-router.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import AppConfig from '../config/app';
 import { isEmptyObject } from '../util/validator';
 import { ParsedUrlQuery } from 'querystring';
-import { WaxService } from '../services/wax-service';
 
 export const WaxRouter = express.Router();
 
@@ -16,18 +15,21 @@ WaxRouter.get('/', async (req, resp) => {
         let reqURL = url.parse(req.url, true);
 
         if(!isEmptyObject<ParsedUrlQuery>(reqURL.query)) {
-            let payload = await WaxService.getWaxByUniqueKey({...reqURL.query});
+            let payload = await waxService.getWaxByUniqueKey({...reqURL.query});
             resp.status(200).json(payload);
-        }else {
-            let payload = await WaxService.getAllWaxes();
-            resp.statusMessage(200).json(payload);
+        } else {
+            let payload = await waxService.getAllWaxes();
+            resp.status(200).json(payload);
         }
-    };
+
+    } catch (e) {
+        resp.status(e.statusCode).json(e);
+    }
 
 WaxRouter.get('/:id', async (req, resp) => {
     const id = +req.params.id; // the plus sign is to type coerce id into a number
     try {
-        let payload = await waxService.getById(id);
+        let payload = await waxService.getWaxByID(id);
         return resp.status(200).json(payload);
     } catch (e) {
         return resp.status(404).json(e).send();
