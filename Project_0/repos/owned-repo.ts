5bfdd9c42e7997bf5477f6be5359ca5b63993444
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-03 15:17:45 -0400

    Created inital ideas for creating repository of owned waxes moving towards implementing brands since it'll be easier.

--- Project_0/repos/owned-repo.ts
+++ Project_0/repos/owned-repo.ts
@@ -13,4 +13,6 @@ import { mapOwnedResultSet } from '../util/result-set-mapper';
 
 export class OwnedRepository implements CrudRepository<Wax> {
     
+    //Need to create a base query to show all of the information of the waxes that are only owned by the user.
+    baseQuery = ``;
 }
