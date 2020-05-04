e3ea919cb9f21b464d62c018e7a8ff8d98d4f212
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-04 19:21:39 -0400

    Finished wax-service tests.

--- src/__tests__/wax-service.test.ts
+++ src/__tests__/wax-service.test.ts
@@ -22,12 +22,11 @@ describe('waxService', () => {
     let sut: WaxService;
     let mockRepo;
 
-    let mockUsers = [
-        new Wax(1, 'aanderson', 'password', 'Alice', 'Anderson', 'Admin'),
-        new Wax(2, 'bbailey', 'password', 'Bob', 'Bailey', 'User'),
-        new Wax(3, 'ccountryman', 'password', 'Charlie', 'Countryman', 'User'),
-        new Wax(4, 'ddavis', 'password', 'Daniel', 'Davis', 'User'),
-        new Wax(5, 'eeinstein', 'password', 'Emily', 'Einstein', 'User')
+    let mockWaxes = [
+        //Tried each possible comibination of waxes with nullable props
+        new Wax(1, 'product', 'brand', 'category', 0.50, true, 5, 'Blank Description'),
+        new Wax(2, 'product2', 'brand', 'category', 0.50, true, 5),
+        new Wax(3, 'product3', 'brand', 'category', 0.50, true)
     ];
 
     beforeEach(() => {
@@ -36,8 +35,8 @@ describe('waxService', () => {
             return {
                 getAll: jest.fn(),
                 getById: jest.fn(),
-                getUserByUniqueKey: jest.fn(),
-                getUserByCredentials: jest.fn(),
+                getWaxByUniqueKey: jest.fn(),
+                getWaxByCredentials: jest.fn(),
                 save: jest.fn(),
                 update: jest.fn(),
                 deleteById: jest.fn()
@@ -45,27 +44,26 @@ describe('waxService', () => {
         });
 
         // @ts-ignore
-        sut = new UserService(mockRepo);
+        sut = new WaxService(mockRepo);
     
     });
 
-    test('should resolve to User[] (without passwords) when getAllUsers() successfully retrieves users from the data source', async () => {
+    test('should resolve to Wax[] when getAllWaxes() successfully retrieves wax from the data source', async () => {
 
         // Arrange
         expect.hasAssertions();
-        mockRepo.getAll = jest.fn().mockReturnValue(mockUsers);
+        mockRepo.getAll = jest.fn().mockReturnValue(mockWaxes);
 
         // Act
-        let result = await sut.getAllUsers();
+        let result = await sut.getAllWaxes();
 
         // Assert
         expect(result).toBeTruthy();
-        expect(result.length).toBe(5);
-        result.forEach(val => expect(val.password).toBeUndefined());
+        expect(result.length).toBe(3);
 
     });
 
-    test('should reject with ResourceNotFoundError when getAllUsers fails to get any users from the data source', async () => {
+    test('should reject with ResourceNotFoundError when getAllWaxes fails to get any waxes from the data source', async () => {
 
         // Arrange
         expect.assertions(1);
@@ -73,7 +71,7 @@ describe('waxService', () => {
 
         // Act
         try {
-            await sut.getAllUsers();
+            await sut.getAllWaxes();
         } catch (e) {
 
             // Assert
@@ -82,29 +80,28 @@ describe('waxService', () => {
 
     });
 
-    test('should resolve to User when getUserById is given a valid an known id', async () => {
+    test('should resolve to Wax when getWaxById is given a valid an known id', async () => {
 
         // Arrange
-        expect.assertions(3);
+        expect.assertions(2);
         
         Validator.isValidId = jest.fn().mockReturnValue(true);
 
         mockRepo.getById = jest.fn().mockImplementation((id: number) => {
-            return new Promise<User>((resolve) => resolve(mockUsers[id - 1]));
+            return new Promise<Wax>((resolve) => resolve(mockWaxes[id - 1]));
         });
 
 
         // Act
-        let result = await sut.getUserById(1);
+        let result = await sut.getWaxByID(1);
 
         // Assert
         expect(result).toBeTruthy();
-        expect(result.id).toBe(1);
-        expect(result.password).toBeUndefined();
+        expect(result.productID).toBe(1);
 
     });
 
-    test('should reject with BadRequestError when getUserById is given a invalid value as an id (decimal)', async () => {
+    test('should reject with BadRequestError when getWaxById is given a invalid value as an id (decimal)', async () => {
 
         // Arrange
         expect.hasAssertions();
@@ -112,7 +109,7 @@ describe('waxService', () => {
 
         // Act
         try {
-            await sut.getUserById(3.14);
+            await sut.getWaxByID(3.14);
         } catch (e) {
 
             // Assert
@@ -121,7 +118,7 @@ describe('waxService', () => {
 
     });
 
-    test('should reject with BadRequestError when getUserById is given a invalid value as an id (zero)', async () => {
+    test('should reject with BadRequestError when getWaxById is given a invalid value as an id (zero)', async () => {
 
         // Arrange
         expect.hasAssertions();
@@ -129,7 +126,7 @@ describe('waxService', () => {
 
         // Act
         try {
-            await sut.getUserById(0);
+            await sut.getWaxByID(0);
         } catch (e) {
 
             // Assert
@@ -138,7 +135,7 @@ describe('waxService', () => {
 
     });
 
-    test('should reject with BadRequestError when getUserById is given a invalid value as an id (NaN)', async () => {
+    test('should reject with BadRequestError when getWaxByID is given a invalid value as an id (NaN)', async () => {
 
         // Arrange
         expect.hasAssertions();
@@ -146,7 +143,7 @@ describe('waxService', () => {
 
         // Act
         try {
-            await sut.getUserById(NaN);
+            await sut.getWaxByID(NaN);
         } catch (e) {
 
             // Assert
@@ -155,7 +152,7 @@ describe('waxService', () => {
 
     });
 
-    test('should reject with BadRequestError when getUserById is given a invalid value as an id (negative)', async () => {
+    test('should reject with BadRequestError when getWaxByID is given a invalid value as an id (negative)', async () => {
 
         // Arrange
         expect.hasAssertions();
@@ -163,7 +160,7 @@ describe('waxService', () => {
 
         // Act
         try {
-            await sut.getUserById(-2);
+            await sut.getWaxByID(-2);
         } catch (e) {
 
             // Assert
@@ -180,7 +177,7 @@ describe('waxService', () => {
 
         // Act
         try {
-            await sut.getUserById(9999);
+            await sut.getWaxByID(9999);
         } catch (e) {
 
             // Assert
